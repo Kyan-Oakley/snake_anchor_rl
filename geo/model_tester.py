3847d6ef3ba64384e0514145b057811f93c67a64
Kyan <koaks3214@gmail.com>
2026-06-22 16:49:52 -0400

    integrated Jsnake into snake options and tuned modified scenes.

--- geo/model_tester.py
+++ geo/model_tester.py
@@ -2,11 +2,12 @@ import mujoco
 import mujoco.viewer
 from pathlib import Path
 
-FILE_PATH = str(Path(__file__).parent / "anchor_scenes" / "tapered_diverging_3deg_9.5cm.xml")
+FILE_PATH = str(Path(__file__).parent / "anchor_scenes" / "tapered_diverging_5deg_9.5cm.xml")
 
 model = mujoco.MjModel.from_xml_path(FILE_PATH)
 data = mujoco.MjData(model)
 model.opt.gravity[:] = [0, 0, 0]
 viewer = mujoco.viewer.launch_passive(model, data)
 while viewer.is_running():
+    mujoco.mj_step(model, data)
     viewer.sync()
